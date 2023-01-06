b631c61f254a29e99bada8aac7544c0d4860b34f
IMTIYAZ13 <imtiyazali828708@gmail.com>
2023-01-06 22:39:37 +0530

    about component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
+import About from './pages/About/about'
 import Home from './views/Home';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      {/* <Route path='/about' element={< About/>} /> */}
+      <Route path='/about' element={< About/>} />
     </Routes>
   );
 };
